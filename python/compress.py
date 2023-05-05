d68118306efe1026f3682d761119576198de556c
Huỳnh Duy Quang <95069794+HuynhDuyQuang20153@users.noreply.github.com>
2023-05-05 21:20:47 +0700

    Update compress.py

--- python/compress.py
+++ python/compress.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 class Compress:
-    def __init__(self, private_key, public_key, orther, direc_cipherText, direc_plaintText, matrix):
+    def __init__(self, private_key, public_key, direc_cipherText, direc_plaintText, matrix):
         self.S = private_key['S']
         self.P = private_key['P']
         self.G = private_key['G']
@@ -15,11 +15,7 @@ class Compress:
 
 
     def Compress_Write(self):
-        # The path to folder where the data is to be saved
         dir_path = os.path.join(os.getcwd(), "..", "data", "Compress_npz")
-
-
-        # Find the largest index of the folder compress_x created
         max_index = 0
         for filename in os.listdir(dir_path):
             if filename.startswith("Compress_folder_") and os.path.isdir(os.path.join(dir_path, filename)):
@@ -27,23 +23,16 @@ class Compress:
                 if index > max_index:
                     max_index = index
 
-
-        # Create a new directory with index max_index + 1
         new_dir_path = os.path.join(dir_path, "Compress_folder_" + str(max_index + 1))
         os.mkdir(new_dir_path)
 
-
-        # File name 
         compress_file = "Compress.npz"
-
-
-        # Create compress file with G, S, P, Gp, Cp, y, yy_, x
         compress_path = os.path.join(new_dir_path, compress_file)
         np.savez_compressed(compress_path, G=self.G, S=self.S, P=self.P, Gp=self.Gp, 
                                 Cp=self.vector_cipher, y=self.ciphertext, yy_=self.vector_plain, 
                                 x=self.plainText, matrix=self.matrix)
+        
         print(f"\t- Save data in: '{compress_path}'")
-
 
         # Show data in '.npz' file
         # data = np.load(compress_path)
