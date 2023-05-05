8bcc91cad35371e8fb0c727aaeb2f6bfffe3d5a3
Huỳnh Duy Quang <95069794+HuynhDuyQuang20153@users.noreply.github.com>
2023-05-05 21:23:29 +0700

    Update txt.py

--- python/txt.py
+++ python/txt.py
@@ -8,11 +8,7 @@ class TXT:
 
 
     def TXT_Write(self):
-        # The path to folder where the data is to be saved
         dir_path = os.path.join(os.getcwd(), "..", "data", "Txt_file")
-
-
-        # Find the largest index of the folder Data_x created
         max_index = 0
         for filename in os.listdir(dir_path):
             if filename.startswith("Txt_folder_") and os.path.isdir(os.path.join(dir_path, filename)):
@@ -20,22 +16,15 @@ class TXT:
                 if index > max_index:
                     max_index = index
 
-
-        # Create a new directory with index max_index + 1
         new_dir_path = os.path.join(dir_path, "Txt_folder_" + str(max_index + 1))
         os.mkdir(new_dir_path)
 
-
-        # The path to '.txt' file
         txt_file = "Data.txt"
         data_path = os.path.join(new_dir_path, txt_file)
 
-
-        # Save data in '.txt' file
-        with open(data_path, "a") as f:
+        with open(data_path, "a", encoding='utf-8') as f:
             f.write("Text Original:\n")
             np.savetxt(f, self.data, fmt="%s", delimiter=", ")
-
             f.write("\nPlainText:\n")
             np.savetxt(f, self.text, fmt="%s", delimiter=", ")
 
